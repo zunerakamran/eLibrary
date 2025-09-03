1333f3d3818fa2b26cd2bf364ab0412c4ef805b3
ZuneraKamran000 <zunerakamran.esipick@gmail.com>
2025-09-03 23:01:08 +0500

    Fix ESLint errors

--- src/app/api/books/[id]/route.ts
+++ src/app/api/books/[id]/route.ts
@@ -12,7 +12,7 @@ interface RouterContext{
     }
 }
 
-export async function GET(req: NextRequest, { params }: RouterContext) {
+export async function GET({ params }: RouterContext) {
     const id = Number(params.id)
     if (Number.isNaN(id)) {
         return NextResponse.json(
@@ -36,7 +36,7 @@ export async function GET(req: NextRequest, { params }: RouterContext) {
     }
 }
 
-export async function DELETE(req: NextRequest, { params }: RouterContext) {
+export async function DELETE(req: NextRequest, { params }: {params:{id:string}}) {
     const id = Number(params.id)
     if (Number.isNaN(id)) {
         return NextResponse.json(
